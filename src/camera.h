42035d80d206d1e8150b8272067039a6791efd9c
Benjamin <benjamin.feldman@student-cs.fr>
2024-06-17 01:29:38 +0200

    reflect

--- src/camera.h
+++ src/camera.h
@@ -4,6 +4,7 @@
 #include "rtweekend.h"
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
 public:
@@ -111,9 +112,12 @@ private:
 
     if (world.hit(r, interval(0.001, infinity), rec)) {
       // vec3 direction = random_on_hemisphere(rec.normal);
-      // below : Lambertian distributed
-      vec3 direction = rec.normal + random_unit_vector();
-      return .5 * ray_color(ray(rec.p, direction), depth - 1, world);
+      ray scattered;
+      color attenuation;
+
+      if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+        return attenuation * ray_color(scattered, depth - 1, world);
+      }
     }
 
     vec3 unit_direction = unit_vector(r.direction());
