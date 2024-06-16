6d5c7c9cbcc3e3075fc65f8ac35571c255180328
Benjamin <benjamin.feldman@student-cs.fr>
2024-06-17 00:26:51 +0200

    lambertian distrib

--- src/camera.h
+++ src/camera.h
@@ -110,7 +110,9 @@ private:
     hit_record rec;
 
     if (world.hit(r, interval(0.001, infinity), rec)) {
-      vec3 direction = random_on_hemisphere(rec.normal);
+      // vec3 direction = random_on_hemisphere(rec.normal);
+      // below : Lambertian distributed
+      vec3 direction = rec.normal + random_unit_vector();
       return .5 * ray_color(ray(rec.p, direction), depth - 1, world);
     }
 
