5ae3b155709a8c1e5ac40c70bbb21ee55d16c17c
Benjamin <benjamin.feldman@student-cs.fr>
2024-06-17 00:40:43 +0200

    gamma

--- src/color.h
+++ src/color.h
@@ -8,7 +8,7 @@ using color = vec3;
 
 inline double linear_to_gamma(double linear_component) {
   if (linear_component > 0)
-    return 1 / sqrt(linear_component);
+    return sqrt(linear_component);
   return 0;
 }
 
